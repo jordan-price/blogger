1457fc5a3352440799704dc8e28ad7f64c3500e8
Jordan Price <jordan@solidcitystudios.com>
2015-03-05 00:49:35 -0700

    Added Models

---
+++ store.js
@@ -0,0 +1,52 @@
+var posts = [
+	{
+		id: '1',
+		title: 'I love blood',
+		body: "Asymmetrical four dollar toast Banksy butcher, sriracha vinyl lomo. DIY squid aesthetic irony, pop-up American Apparel next level 3 wolf moon kitsch bitters Williamsburg distillery Carles. Disrupt yr Pitchfork sriracha, tattooed actually fanny pack. Kitsch chambray master cleanse mumblecore hella pop-up whatever actually, cronut Schlitz McSweeney's deep v freegan. Asymmetrical taxidermy Brooklyn, irony bespoke Kickstarter sustainable PBR put a bird on it ethical shabby chic tilde organic literally trust fund. Semiotics squid DIY stumptown. Occupy ethical typewriter biodiesel listicle, disrupt VHS wayfarers beard mumblecore."
+	},
+	{
+		id: '2',
+		title: "Yeep yep",
+		body: "Asymmetrical four dollar toast Banksy butcher, sriracha vinyl lomo. DIY squid aesthetic irony, pop-up American Apparel next level 3 wolf moon kitsch bitters Williamsburg distillery Carles. Disrupt yr Pitchfork sriracha, tattooed actually fanny pack. Kitsch chambray master cleanse mumblecore hella pop-up whatever actually, cronut Schlitz McSweeney's deep v freegan. Asymmetrical taxidermy Brooklyn, irony bespoke Kickstarter sustainable PBR put a bird on it ethical shabby chic tilde organic literally trust fund. Semiotics squid DIY stumptown. Occupy ethical typewriter biodiesel listicle, disrupt VHS wayfarers beard mumblecore."
+	},
+	{
+		id: '3',
+		title: "Zizzle Zazzle",
+		body: "Asymmetrical four dollar toast Banksy butcher, sriracha vinyl lomo. DIY squid aesthetic irony, pop-up American Apparel next level 3 wolf moon kitsch bitters Williamsburg distillery Carles. Disrupt yr Pitchfork sriracha, tattooed actually fanny pack. Kitsch chambray master cleanse mumblecore hella pop-up whatever actually, cronut Schlitz McSweeney's deep v freegan. Asymmetrical taxidermy Brooklyn, irony bespoke Kickstarter sustainable PBR put a bird on it ethical shabby chic tilde organic literally trust fund. Semiotics squid DIY stumptown. Occupy ethical typewriter biodiesel listicle, disrupt VHS wayfarers beard mumblecore."
+	},
+	{
+		id: '4',
+		title: "How How Do Do!",
+		body: "Asymmetrical four dollar toast Banksy butcher, sriracha vinyl lomo. DIY squid aesthetic irony, pop-up American Apparel next level 3 wolf moon kitsch bitters Williamsburg distillery Carles. Disrupt yr Pitchfork sriracha, tattooed actually fanny pack. Kitsch chambray master cleanse mumblecore hella pop-up whatever actually, cronut Schlitz McSweeney's deep v freegan. Asymmetrical taxidermy Brooklyn, irony bespoke Kickstarter sustainable PBR put a bird on it ethical shabby chic tilde organic literally trust fund. Semiotics squid DIY stumptown. Occupy ethical typewriter biodiesel listicle, disrupt VHS wayfarers beard mumblecore."
+	},
+
+];
+
+var comments = [
+	{
+		id: '1',
+		postID: '3',
+		text: "Asymmetrical four dollar toast Banksy butcher, sriracha vinyl"
+	},
+	{
+		id: '2',
+		postID: '3',
+		text: "four dollar toast Banksy butcher, sriracha vinyl"
+	},
+	{
+		id: '3',
+		postID: '1',
+		text: "dollar toast Banksy butcher, sriracha vinyl"
+	},
+	{
+		id: '4',
+		postID: '1',
+		text: "toast Banksy butcher, sriracha vinyl"
+	},
+	{
+		id: '5',
+		postID: '2',
+		text: "Banksy butcher, sriracha vinyl"
+	},
+
+];
